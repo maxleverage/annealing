1799837e3b4a96affd1c4a32d50a8685503ef2a4
maxleverage <henrywang7@gmail.com>
2013-11-20 06:11:28 -0800

    Added raw input features

--- py_annealing_search.py
+++ py_annealing_search.py
@@ -6,6 +6,8 @@ import numpy as np
 import time
 
 """ Triples search """
+""" Search for integer p less than max_p which generates the longest set of Pythagorean triples """
+
 def triples_seq_search(max_p):
 	start = time.time()
 	perimeter_range = range(2,max_p,2)
@@ -21,51 +23,64 @@ def triples_seq_search(max_p):
 			result = p
 	return result, time.time() - start
 
-""" Simulated Annealing """
-""" Acceptance probability function for simulated annealing """
-
-def acceptance_probability(temperature, init_energy, new_energy):
-	""" If neighboring energy level is lower, we accept it unconditionally else we compute the transition probability as a function of the initial energy and current temperature"""
-	temperature = float(temperature)
-	init_energy = float(init_energy)
-	new_energy = float(new_energy)
-	if new_energy < init_energy:
-		return 1
-	else:
-		return m.exp((init_energy - new_energy) / temperature)
-
 """ Compute energy of solution """
 
 def compute_energy(perimeter):
 	""" Takes solution index and computes the energy associated at that index """
 	initial_energy = 0
-	for a in range(1, int(m.ceil(perimeter/3))):
+	for a in range(1, int(m.ceil(float(perimeter)/3))):
 		if perimeter * (perimeter - 2 * a) % (2 * (perimeter - a)) == 0:
 			initial_energy -= 1
-	return initial_energy
+	return float(initial_energy)
 
-""" Simulated annealing search for pythagorean triples """
+""" Vectorised implementation of pythagorean triples counter. Wins for large values of perimeter """
 
-def annealing_search(start_temp, end_temp, cooling_rate, max_p, m_iter):
+def compute_energy_vec(perimeter):
+	""" Vectorised implementation of computing the energy of a state """
+	a_val = np.array(range(1,int(float(perimeter)/3)))
+	result = perimeter * (perimeter - 2.0 * a_val) % (2.0 * (perimeter - a_val))
+	return float(-len(np.where(result==0)[0]))
+
+""" Annealing search """
+print "Running simulated annealing"
+
+def annealing_search(start_temp, cooling_rate, max_p, max_iter):
 	start = time.time()
-	temperature = start_temp
-	""" Search space energy """
 	perimeter_range = range(2, max_p, 2)
-	initial_solution = random.randint(0, len(perimeter_range)-1)
-	initial_energy = compute_energy(perimeter_range[initial_solution])
-	best_solution = [initial_solution]
-	while temperature > end_temp:
-		m_counter = 0
-		while m_counter < m_iter:
-			new_solution = random.randint(0, len(perimeter_range)-1) 
-			new_energy = compute_energy(perimeter_range[new_solution])
-			if new_energy < initial_energy:
-				initial_energy = new_energy
-				initial_solution = new_solution
-			elif acceptance_probability(temperature, initial_energy, new_energy) > random.random():
-				initial_energy = new_energy
-				initial_solution = new_solution
-			m_counter += 1		
-		temperature *= 1 - cooling_rate
-		return perimeter_range[initial_solution]
+	initial_state = np.random.randint(0,len(perimeter_range))
+	initial_energy = compute_energy_vec(perimeter_range[initial_state])
+	iter = 0
+	while iter < max_iter:
+		new_state = np.random.randint(0, len(perimeter_range)) 	
+		new_energy = compute_energy_vec(perimeter_range[new_state])
+		if new_energy < initial_energy:
+			initial_energy = new_energy
+			initial_state = new_state
+			start_temp *= 1 - cooling_rate
+		elif new_energy == initial_energy:
+			start_temp = start_temp * (1- cooling_rate/5)
+		elif np.exp((initial_energy - new_energy)/start_temp) > np.random.random():
+			initial_energy = new_energy
+			initial_state = new_state
+			start_temp *= 1 - cooling_rate
+		else:
+			start_temp = start_temp * (1 - cooling_rate/5)
+		iter += 1
+	return (initial_state+1)*2, time.time() - start
+
+""" Raw Inputs """
+
+start_temp = float(raw_input('Enter start temperature: '))
+cooling_rate = float(raw_input('Enter cooling rate: '))
+max_p = int(raw_input('Enter maximum perimeter: '))
+max_iter = int(raw_input('Enter maximum iteration: '))
+counter_limit = int(raw_input('Enter SA runs: '))
+
+""" Annealing iterations """
+counter = 0
+while counter < counter_limit:
+	print annealing_search(start_temp, cooling_rate, max_p, max_iter)
+	counter += 1
+
+
 
